f88142cd1f55092b8cadee3124d276bb923c680e
Julietta Torres-Londono <gugiyumyum@gmail.com>
2025-05-31 14:49:10 -0700

    basic javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = 'Julietta';
+let age = 14;
+let likesSushi = true;
+
+function greeting(name) {
+    console.log('Hello', name);
+}
+
+greeting('Julietta');
+
+let newName = document.getElementById("greeter").value;
