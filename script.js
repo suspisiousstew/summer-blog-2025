0947f1b37c08ec00c04c52a36e81b6134cabe35c
Julietta Torres-Londono <gugiyumyum@gmail.com>
2025-06-07 14:55:49 -0700

    updateeee

--- script.js
+++ script.js
@@ -13,5 +13,13 @@ function saveName() {
 
     let newName = document.getElementById("greeter").value; // text field value from the user
     document.getElementById('username').textContent = newName;
-    document.getElementById("greeter").value ="";
+    document.getElementById("greeter").value = "";
 }
+
+document.querySelectorAll('.accordion-header').forEach(function (button) {
+    button.addEventListener('click', function () {
+        let content = this.nextElementSibling;
+        content.style.display = (content.style.display === 'block') ? 'none' : 'block';
+    });
+});
+
